78ab2d9a4aeba43611d0360b99c2eefbda9de6c8
Jamil-Habash <168636758+Jamil-Habash@users.noreply.github.com>
2026-08-04 14:40:01 +0300

    Add files via upload

---
+++ HTML_CSS_JS/Loop_Challenge.js
@@ -0,0 +1,30 @@
+//1- Display odd numbers
+/*for(var i = 1; i <= 20; i+=2) {
+    console.log(i);
+}*/
+
+//Decreasing Multiples of 3
+/*for(var i = 100; i >= 0; i--) {
+    if(i % 3 == 0){
+        console.log(i);
+    }
+}*/
+
+//Print the given sequence
+/*for(var i = 4; i >= -3.5; i-=1.5) {
+    console.log(i);
+}*/
+
+//Sigma
+var sum = 0;
+for(var i = 1; i <= 100; i++) {
+    sum += i;
+}
+console.log(sum);
+
+//Factorial
+var product = 1;
+for(var i = 1; i <= 12; i++) {
+    product *= i;
+}
+console.log(product);
